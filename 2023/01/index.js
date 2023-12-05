7468029fe51f9b746f64f985cec0f3980a98ab98
Lix Luthor <689071+rezemble@users.noreply.github.com>
2023-12-05 16:29:51 +0100

    add day 1 part 2

--- 2023/01/index.js
+++ 2023/01/index.js
@@ -2,14 +2,44 @@ import fs from "fs";
 import path from "path";
 
 const INPUT_SRC = path.resolve(process.cwd(), "input.txt");
+const LUT = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+const getNumbers = (str) => {
+  if (!str.length) return [];
+  const numAt = +str.slice(0, 1);
+  if (!isNaN(numAt)) return [numAt, ...getNumbers(str.slice(1))];
+  const wordAt = LUT.find((s) => str.startsWith(s));
+  if (wordAt) return [LUT.indexOf(wordAt), ...getNumbers(str.slice(1))];
+  return getNumbers(str.slice(1));
+};
 fs.promises
   .readFile(INPUT_SRC, "utf-8")
-  .then((r) =>
-    r
-      .trim()
-      .split("\n")
+  .then((r) => {
+    const lines = r.trim().split("\n");
+
+    const A = lines
       .map((l) => l.replace(/[^0-9]/g, ""))
       .map((n) => +[n.slice(0, 1), n.slice(-1)].join(""))
-      .reduce((a, b) => a + b, 0)
-  )
-  .then((x) => console.log(`result is [%s]!`, x));
+      .reduce((a, b) => a + b, 0);
+    const B = lines
+      .map((l) => {
+        const n = getNumbers(l);
+        console.log(l, n.join(""));
+        return n;
+      })
+      .map((n) => +[n.at(0), n.at(-1)].join(""))
+      .reduce((a, b) => a + b, 0);
+    return [A, B];
+  })
+  .then(([a, b]) => console.log(`result is [%s] - part 2 is [%s]!`, a, b));
